a9fd5e7c93061811825f0490b147baecf517cf3e
kundan kumar <kundanchauhan1997@gmail.com>
2023-07-05 06:18:47 +0530

    Added: Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to addressBook program");
+        }   
+    }
+}
